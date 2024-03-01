7ad600e73d760d3c8b1eb24e709b383485fa3dc6
TournesolTangible <99299545+TournesolTangible@users.noreply.github.com>
2024-03-01 11:45:46 -0600

    Added textures and some objects to main scene

---
+++ Scripts/r_hard_point1.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("lmb"):
+		
+		transform = $point_position.global_transform
+		print(transform)
